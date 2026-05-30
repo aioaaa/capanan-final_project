2746774a62e507ca16c42e622362037ca86812f9
Maricon Capanan <mariconcapanan2004@gmail.com>
2026-05-30 10:04:24 +0800

    Fix table name

--- app/Models/ToDo.php
+++ app/Models/ToDo.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ToDo extends Model
 {
     //
-    protected $table = 'todos';
+    protected $table = 'todo';
     protected $fillable = [
         'user_id',
         'product_name',
