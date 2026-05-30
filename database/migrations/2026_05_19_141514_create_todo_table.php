f13ee3cbd0cb00714a967de191c05b3345efc3be
Maricon Capanan <mariconcapanan2004@gmail.com>
2026-05-30 15:12:30 +0800

    Nagdagdag ng table

--- database/migrations/2026_05_19_141514_create_todo_table.php
+++ database/migrations/2026_05_19_141514_create_todo_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->string('task');
+            $table->string('product_name');
+            $table->string('category');
+            $table->integer('stock');
+            $table->decimal('price', 8, 2);
             $table->timestamps();
             // set it as FK -> id from other table -> from table
             // -> onDelete will delete all records
